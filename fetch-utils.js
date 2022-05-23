0c94bd5d5dded5e9b4fa983b0a45227924cfc73a
jenna-graham <jclayto2@gmail.com>
2022-05-23 12:21:43 -0700

    set up supabase, linked to vs code, added getFamilies function to fetch all families and their bunnies

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://icyrbwltlybmigeljxmh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljeXJid2x0bHlibWlnZWxqeG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMDMxMzMsImV4cCI6MTk2Nzg3OTEzM30.kUzjWO4wi0A9AVcWdFt_BG9uq-HBZoAR2aZ4IRN55yw';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -9,6 +9,12 @@ export function getUser() {
 }
 
 export async function getFamilies() {
+    const response = await client.from('loving_families').select('*, fuzzy_bunnies(*)');
+    if (response.error) {
+        console.error(response.error.message);
+    } else {
+        return response.data;
+    }
     // fetch all families and their bunnies
 
     return checkError(response);
