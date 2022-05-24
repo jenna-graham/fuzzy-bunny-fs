5103165846e8885ea771cc349d9c56b769f60c0e
jenna-graham <jclayto2@gmail.com>
2022-05-23 17:13:13 -0700

    added event listener to create new bunny

--- create/create.js
+++ create/create.js
@@ -9,22 +9,36 @@ const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async e => {
-    // prevent default
+    e.preventDefault();
 
+    // prevent default
+    const bunnyForm = new FormData(form);
+    console.log(bunnyForm.get('family-id')); 
+    await createBunny({
+        name: bunnyForm.get('bunny-name'),
+        family_id: bunnyForm.get('family-id'),
+    });
     // get the name and family id from the form
 
     // use createBunny to create a bunny with this name and family id
-    
+    window.location.href = '/families';
     form.reset();
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-
+    const familyId = document.getElementById('family-id');
     // go get the families from supabase
+    const families = await getFamilies();
 
     // for each family
+    for (let family of families) {
+        const option = document.createElement('option');
+        option.value = family.id;
+        option.textContent = family.name;
+        familyId.append(option);
+    }
 
     // create an option tag
 
